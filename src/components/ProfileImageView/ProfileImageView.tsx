4b3de7c943a43cdcc7961fe316e386729e6854d0
lukas-burda <contatolukasferreira@gmail.com>
2025-07-16 01:19:32 -0300

    adjust image -profile-foto css

--- src/components/ProfileImageView/ProfileImageView.tsx
+++ src/components/ProfileImageView/ProfileImageView.tsx
@@ -7,7 +7,7 @@ interface ProfileImageProps {
 
 export const ProfileImageView: React.FC<ProfileImageProps> = ({ src, alt }) => {
   return (
-    <div className="cursor-pointer relative w-36 h-36 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 my-5 rounded-full overflow-hidden border-2 border-cyan-400 shadow-lg transition-transform hover:scale-105 mx-auto">
+    <div className="cursor-pointer w-36 h-36 sm:w-48 sm:h-48 md:w-56 md:h-56 lg:w-64 lg:h-64 my-5 rounded-full overflow-hidden border-2 border-cyan-400 shadow-lg transition-transform hover:scale-105 mx-auto">
       <img
         
         src={src}
